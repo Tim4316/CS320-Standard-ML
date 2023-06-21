83df1e74eccbc68622853ddb572527410ca26496
Tim4316 <112959814+Tim4316@users.noreply.github.com>
2023-06-20 20:35:24 -0400

    Add files via upload

--- assigns/03/MySolution/Python/assign03_03.py
+++ assigns/03/MySolution/Python/assign03_03.py
@@ -20,3 +20,28 @@ def word_neighbors(word):
     raise NotImplementedError
 #
 ####################################################
+"""val word_neighbors = fn (word: string) =>
+  let
+    val len = string_length word
+    fun char_variant (i, ch) j = 
+      if ch = strsub(AB, j)
+      then NONE
+      else 
+        SOME (string_tabulate (len, fn k => 
+            if k = i 
+            then strsub(AB, j) 
+            else strsub(word, k)))
+    fun char_variants (i, ch) = 
+        list_map (list_filter (list_tabulate (26, char_variant (i, ch)), optn_is_some), optn_unsome)
+  in
+    list_concat (string_imap_list(word, char_variants))
+  end"""
+def word_neighbors(word):
+    result = fnlist_nil()
+    for i in range(len(word)):
+        for letter in 'abcdefghijklmnopqrstuvwxyz':
+            if word[i] != letter:
+                neighbor = word[:i] + letter + word[i+1:]
+                result = fnlist_cons(neighbor, result)
+
+    return result
